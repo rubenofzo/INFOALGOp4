ce1e93e2e524f53e75e1f33dd34f89e1a2d227af
Ruben <r.j.b.deboer@students.uu.nl>
2025-06-27 13:00:08 +0200

    fix: speed optimisation is now actually aplied

--- Program.cs
+++ Program.cs
@@ -1,73 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-/*
- specification:
-De invoer begint met een regel met drie getallen n ≤
-   1.000.000, m ≤ 10.000.000 en s ≤ n. Hierbij is n het aantal
-   kamers k1, . . . , kn, m het aantal gangen en s het totale aantal
-   zilveren munten in de dungeon.
-   Daarna volgen m regels met ieder 2 getallen. Een regel met de getallen (a, b) geeft aan
-   dat er een eenrichtingsgang van kamer ka naar kamer kb is (het kan zijn dat er zowel een
-   gang van a naar b als een gang van b naar a is).
-   Tot slot volgt een regel met s verschillende getallen c1, . . . , cs, waarbij het getal ci
-   aangeeft dat er een zilveren munt te vinden is in kamer kci (en in iedere kamer is hoogstens
-   ´e´en munt te vinden).
-   De ingang van de dungeon is kamer 1; de uitgang kamer n.
-   
-   Op de uitvoer komt het getal Z, het maximale aantal munten dat Pones op een wan-
-   deling van de ingang naar de uitgang kan verzamelen. Daarna volgt een regel met Z
-   verschillende getallen X1, . . . XZ , waarbij Xi zodanig is dat er een zilveren munt te vinden
-   is in kamer kXi , en dat als i < j er een pad bestaat van kamer KXi naar kamer KXj . Ook
-   moet er een pad bestaan van K1 (de ingang) naar KX1 en een pad van KXZ naar Kn (de
-   uitgang). Als er geen pad van K1 naar Kn bestaat, geef dan als uitvoer het getal Z = 0
-   gevolgd door een lege regel.
-   Merk op dat een wandeling niet simpel hoeft te zijn: je kunt een bepaalde kamer meer
-   dan eens bezoeken (maar een zilveren munt kun je slechts ´e´en keer meenemen).
-   
-   Algoritmische Aanwijzingen
-   Kijk naar de relaties tussen het al dan niet oppakken van bepaalde munten en andere
-   munten. In het voorbeeld (Figuur 1) is het bijvoorbeeld zo dat als je munt 9 pakt je nooit
-   munt 6 kan pakken, maar als je munt 2 pakt kun je ook altijd munt 3 pakken en als je
-   munt 10 pakt, kun je ook altijd munt 9 pakken.
-   Je algoritme moet lineair zijn in de lengte van de invoer.
-   De uitvoer is niet noodzakelijkerwijs uniek bepaald. DomJudge accepteert iedere geldige
-   oplossing. In het voorbeeld zou onder meer 2 3 4 5 9 10 8 ook geldig zijn.
- */
+﻿
 
 namespace Dungeon
 {
     using System;
     using System.Collections.Generic;
-    using System.Reflection;
-    using System.Security.Cryptography.X509Certificates;
 
     class Program
     {
-        // Function for graph representation
-        static void addEdge(List<List<KeyValuePair<int, int>>> adj, int u, int v, int w)
-        {
-            adj[u].Add(new KeyValuePair<int, int>(v, w));
-        }
-
         static void displayAdjList(Dictionary<int,List<int>> adj)
         {
-
-            foreach (var j in adj.Keys)
-            {
-                foreach (var i in adj[j])
-                    continue;
-                    //Console.Write("{" + j + ", " + i + "} ");
-            }
-            //Console.WriteLine();
+            // foreach (var j in adj.Keys)
+            // {
+            //     foreach (var i in adj[j])
+            //         Console.Write("{" + j + ", " + i + "} ");
+            // }
+            // Console.WriteLine();
         }
         static void displayTable(Dictionary<int,(int,int)> table)
         {
-            
-            foreach (int j in table.Keys)
+            // foreach (int j in table.Keys)
+            // {
+            //     (int a, int b) = table[j];
+            //     Console.Write(j + ": {" + a + ", " + b + "} ");
+            // }
+            // Console.WriteLine();
+        }
+        
+        static (int n, int m, int s, List<(int from, int to)> corridors, int[] coinRooms) ParseInput()
+        {
+            // Read first line with n, m, s
+            string[] firstLine = Console.ReadLine().Split(' ');
+            int n = int.Parse(firstLine[0]); // number of rooms
+            int m = int.Parse(firstLine[1]); // number of corridors
+            int s = int.Parse(firstLine[2]); // number of silver coins
+            // Read m lines of corridors
+            var corridors = new List<(int from, int to)>();
+
+            for (int i = 0; i < m; i++)
             {
-                (int a, int b) = table[j];
-                //Console.Write(j + ": {" + a + ", " + b + "} ");
+                string[] corridor = Console.ReadLine().Split(' ');
+                int from = int.Parse(corridor[0]);
+                int to = int.Parse(corridor[1]);
+                corridors.Add((from, to));
             }
-            //Console.WriteLine();
+            // Read last line with coin locations
+            string[] coinLine = Console.ReadLine().Split(' ');
+            List<int> parsedCoinLine = coinLine.Select(int.Parse).ToList();
+            int[] coinRooms = new int[n + 1];
+
+            foreach (int i in parsedCoinLine)
+            {
+                coinRooms[i] = 1;
+            }
+
+
+            return (n, m, s, corridors, coinRooms);
         }
 
         // adjacency list, n entries for each room. Each entry has a list of possible destinations and a 0 or 1 for a coin
@@ -107,43 +94,12 @@ namespace Dungeon
             // coinRooms = set of rooms with coins
             var (n, m, s, corridors, coinRooms) = ParseInput();
             var (adj,revadj) = setupAdj(n, m, corridors);
-            displayAdjList(adj); //TODO remove
-            
+            displayAdjList(adj);
             solve(n,m,corridors,coinRooms,adj,revadj);
-
             
         }
 
-        static (int n, int m, int s, List<(int from, int to)> corridors, int[] coinRooms) ParseInput()
-        {
-            // Read first line with n, m, s
-            string[] firstLine = Console.ReadLine().Split(' ');
-            int n = int.Parse(firstLine[0]); // number of rooms
-            int m = int.Parse(firstLine[1]); // number of corridors
-            int s = int.Parse(firstLine[2]); // number of silver coins
-            // Read m lines of corridors
-            var corridors = new List<(int from, int to)>();
 
-            for (int i = 0; i < m; i++)
-            {
-                string[] corridor = Console.ReadLine().Split(' ');
-                int from = int.Parse(corridor[0]);
-                int to = int.Parse(corridor[1]);
-                corridors.Add((from, to));
-            }
-            // Read last line with coin locations
-            string[] coinLine = Console.ReadLine().Split(' ');
-            List<int> parsedCoinLine = coinLine.Select(int.Parse).ToList();
-            int[] coinRooms = new int[n+1];
-
-            foreach (int i in parsedCoinLine)
-            {
-                coinRooms[i] = 1;
-            }
-
-
-            return (n, m, s, corridors, coinRooms);
-        }
 
         static void solve(int amountOfRooms, int amountOfCorridors, List<(int from, int to)> corridors, int[] coinRooms, Dictionary<int, List<int>> adj, Dictionary<int, List<int>> revadj)
         {
@@ -151,13 +107,13 @@ namespace Dungeon
             // reachability: filter all nodes that are not on a path
             (Dictionary<int, List<int>> nadj, Dictionary<int, List<int>> nrevadj) = bfs(amountOfRooms, corridors, adj, revadj);
             // if end and start not connected, return
-            if (!(nadj.ContainsKey(1) && revadj.ContainsKey(amountOfRooms))){
+            if (!(nadj.ContainsKey(1) && nrevadj.ContainsKey(amountOfRooms))){
                Console.WriteLine("0");
                Console.WriteLine();
                return; }
 
             // Find SCC's (turns problem into a DAG)
-            (int[] starttimesG, int[]endtimesG,List<int> ts) = findSCC(amountOfRooms, corridors, adj, revadj);
+            (int[] starttimesG, int[]endtimesG,List<int> ts) = findSCC(amountOfRooms, corridors, nadj, nrevadj);
             (Dictionary<int, List<int>> adjSCC,Dictionary<int, List<int>> revadjSCC,int[]coinrooms,var sccToRoom) = processSCC(corridors,starttimesG,endtimesG,amountOfRooms,amountOfCorridors,coinRooms);
             //dynprog through sccs
             Dictionary<int, (int, int)>  distanceTable = SSSP(ts,adjSCC,revadjSCC,amountOfRooms,coinrooms);
